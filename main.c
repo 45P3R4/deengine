f26aa2ec8ec2ca4542ed741e92c25b097e9b1093
KristinaSamoilova <51315442+45P3R4@users.noreply.github.com>
2023-05-16 01:11:55 +0300

    vertex indexes wip

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
 
 #include "draw_primitives.h"
 #include "window.h"
@@ -36,11 +37,7 @@ int main()
 
     glFrustum(-1,1, -1,1, 1,100);
 
-    open_obj("models\\robo.obj");
-    // for (int i = 0; i < obj_v_count; i++)
-    // {
-    //     printf("%u %g\n",i, obj_vert[i]);
-    // }
+    open_obj("models\\cube.obj");
 
     
     while (!glfwWindowShouldClose(window))
@@ -62,22 +59,15 @@ int main()
             glEnableClientState(GL_VERTEX_ARRAY);
             glColor3f(1,1,1);
                 glDrawArrays(GL_POINTS, 0, obj_v_count/3);
+                glColor3f(0.9,0.9,0.9);
+                glDrawArrays(GL_LINE_LOOP, 0, obj_v_count/3);
+                glColor3f(0.8,0.8,0.8);
+                glDrawArrays(GL_TRIANGLE_FAN, 0, obj_v_count/3);
             glDisableClientState(GL_VERTEX_ARRAY);
-
-            // glBegin(GL_TRIANGLE_FAN);
-            //     glVertex3f(obj_vert[0],obj_vert[1],obj_vert[2]);
-            //     glVertex3f(obj_vert[3],obj_vert[4],obj_vert[5]);
-            //     glVertex3f(obj_vert[6],obj_vert[7],obj_vert[8]);
-            //     glVertex3f(obj_vert[9],obj_vert[10],obj_vert[11]);
-            //     glVertex3f(obj_vert[12],obj_vert[13],obj_vert[14]);
-            //     glVertex3f(obj_vert[15],obj_vert[16],obj_vert[17]);
-            //     glVertex3f(obj_vert[18],obj_vert[19],obj_vert[20]);
-            //     glVertex3f(obj_vert[21],obj_vert[22],obj_vert[23]);
-            // glEnd();
 
         glPopMatrix();
 
-        //glFlush();
+        glFlush();
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
--- model_obj.h
+++ model_obj.h
@@ -1,14 +1,28 @@
 GLfloat* obj_vert = NULL;
+GLfloat* obj_index = NULL;
 unsigned int obj_v_count = 0;
 
 
 void open_obj(char* path)
 {
-    char c[1000] = {0};
+    char cv[100] = {0};
+    char cn[100] = {0};
+    char ct[100] = {0};
+    char cf[100] = {0};
+    char cs[100] = {0};
+    char c;
+
     float v_x;
     float v_y;
     float v_z;
-    
+
+    int index[30] = {0};
+    const char MAX_INDEXES = 10;
+    char index0[MAX_INDEXES];
+    char index1[MAX_INDEXES];
+    char index2[MAX_INDEXES];
+    char index3[MAX_INDEXES];
+
     
     if(fopen(path,"r")){
         FILE* obj;
@@ -17,9 +31,9 @@ void open_obj(char* path)
         obj_vert = malloc(sizeof(GLfloat));
 
         obj = fopen(path,"r");
-        while(fscanf(obj, "%s %g %g %g", c, &v_x, &v_y, &v_z) != EOF)
+        while(fscanf(obj, "%s %g %g %g", cv, &v_x, &v_y, &v_z) != EOF)
         {
-            if(strcmp(c, "v") == 0){
+            if(strcmp(cv, "v") == 0){
                 obj_v_count += 3; 
                 obj_vert = realloc(obj_vert, obj_v_count * sizeof(GLfloat));
                 *(obj_vert+i) = v_x;
@@ -29,7 +43,44 @@ void open_obj(char* path)
             }
             else
                 if(i > 0)
-                    break;
+                    break; 
+        }
+
+        while(fscanf(obj, "%s", cn) != EOF)
+        {
+            if(strcmp(cn, "vt") == 0)
+                break;
+            printf("%s\n", cn);
+        }
+        printf("\n");
+
+        while(fscanf(obj, "%s", ct) != EOF)
+        {
+            if(strcmp(ct, "s") == 0)
+                break;
+            printf("%s\n", ct);
+        }
+        printf("\n");
+
+        fscanf(obj, "%s", cs);
+
+        while(fscanf(obj, "%s %s %s %s %s", cf, index0, index1, index2, index3) != EOF)
+        {
+            printf("%s %s %s %s %s\n", cf, index0, index1, index2, index3);
+            unsigned int counter = 0;
+            for (int i = 0; i < MAX_INDEXES; i++)
+            {
+                if(isdigit(index0[i])){
+                    index[counter] = (index0[i] - '0');
+                }
+                else if(isdigit(index0[i]) && index[counter] != 0){
+                    index[counter] = 10 * index[counter] + index0[i];
+                }
+                else if(index0[i] = '/')
+                    continue;
+                
+                printf("\n%d\n", index[i]);
+            }
         }
         fclose(obj);
     } 
